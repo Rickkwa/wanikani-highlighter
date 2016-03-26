4bedc199746652df5c93a3c242c1baa4cb00c8c5
Ricky K <rickyk.162@gmail.com>
2016-03-26 15:38:58 -0400

    Set opacity of background highlight

--- js/highlight.js
+++ js/highlight.js
@@ -1,7 +1,7 @@
 if (!isExcludedSite() && containsJapanese($("body").text())) {
 	getWordList(function(wordList) {
 		var regex = new RegExp(wordList.join("|"), "g");
-		$("body *").replaceText(regex, wrapText);
+		$("*").replaceText(regex, wrapText);
 	});
 } else { console.log("WKH", "Ignore page"); }
 
@@ -22,10 +22,31 @@ function containsJapanese(str) {
 }
 
 function wrapText(str) {
-	var hlColor = "cyan";
+	var hlColor = "#00ffff"; // cyan
 	var hlWrap = $("<span>").html(str).addClass("wkh-highlight");
-	hlWrap = hlWrap.css("background-color", hlColor);
 	// TODO: change text color if needed (so not similar to hlColor)
+	var rgb = hexToRgb(hlColor);
+	var opacity = 0.3;
+	hlWrap = hlWrap.css({
+		"background-color": "rgba("+rgb.r+","+rgb.g+","+rgb.b+","+opacity+")"
+	});
 	return hlWrap.wrap("<span>").parent().html();
 }
 
+
+
+// http://stackoverflow.com/a/5624139/2079781
+function hexToRgb(hex) {
+	// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+		return r + r + g + g + b + b;
+	});
+
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result ? {
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16)
+	} : null;
+}
